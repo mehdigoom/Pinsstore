d10742c8aee27e09e57c888c39a34bece7caa981
mehdigoom <medhi.gay@sfr.fr>
2019-07-08 15:09:01 +0200

    panier ok

--- src/App.js
+++ src/App.js
@@ -11,20 +11,37 @@ class App extends React.Component {
         ID: "",
          Username: "",
          passeword:"",
-         nombrearticle:"",
+         nombrearticle:0,
          IDarticle:"",
+         action:0
       }
+      this.ajoutarticle= this.ajoutarticle.bind(this);
+      this.retirarticle= this.retirarticle.bind(this);
    }
 
+  
+ajoutarticle(){
+
+
+  this.setState({nombrearticle: this.state.nombrearticle+1})
+
+}
+retirarticle(){
+
+ 
+    this.setState({nombrearticle: this.state.nombrearticle-1})
+  
+  
+  }
 
    render() {
       return (
          <div>
-
+<p>article dans mon painer {this.state.nombrearticle}</p>
             <h1>Slider</h1>
             <h2>Article</h2>
-            <button>Ajouter 1 au panier</button>
-            <button>Ajouter 1 au retirer</button>
+            <button onClick={this.ajoutarticle}>Ajouter 1 au panier</button>
+            <button onClick={this.retirarticle}> Ajouter 1 au retirer</button>
          </div>
       );
    }
