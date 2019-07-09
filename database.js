dd3a93d76f51088816e4a0b9a616af327b15e10c
lamisy93 <ami.sy@hotmail.fr>
2019-07-09 11:52:47 +0200

    'update'

---
+++ database.js
@@ -0,0 +1,46 @@
+var mysql = require('mysql');
+  
+var client= mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'peanshop',
+});
+  
+client.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+  });
+
+const end = function end() {
+  client.end();
+};
+
+const getUsers = function getUsers(clbk, id) {
+  let sql;
+
+  if (id) 
+    sql = `SELECT * FROM users WHERE id = ?`;
+
+  else 
+    sql = `SELECT * FROM users`;
+  
+  const query = client.query(sql, [id], (error, results, fields) => {
+      if (error) return clbk(error, null);
+      return clbk(null, results);
+  });
+  console.log("Last Query :", query.sql);
+}
+
+const getProducts = function getProducts(clbk, id) {
+  let sql = "SELECT * FROM products";
+  client.query(sql, [id], (error, results, fields) => {
+      if (error) return clbk(error, null);
+      return clbk(null, results);
+  });
+}
+
+module.exports = {
+  getUsers,
+  getProducts,
+};  
--- index.js
+++ index.js
@@ -1,14 +1,36 @@
 const http = require('http');
-
+const database = require("./database");
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = 5000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  res.end();
 });
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+var express = require('express');
+var app = express();
+
+var bodyParser = require("body-parser"); 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/users', (req, res) => {
+  database.getUsers( (err, users) => {
+    if (err) return res.status(500).send(err);
+    return res.status(200).send(users);
+  }, null);
+});
+
+app.get('/products', (req, res) => {
+  database.getProducts( (err, products) => {
+    if (err) return res.status(500).send(err);
+    return res.status(200).send(products);
+  }, null);
+});
+
