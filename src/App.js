c9f2049f08db9501816eae9e778e9e3ee30affdc
mehdigoom <medhi.gay@sfr.fr>
2019-07-09 10:07:41 +0200

    Update article

--- src/App.js
+++ src/App.js
@@ -20,33 +20,45 @@ class App extends React.Component {
       this.viderarticle= this.viderarticle.bind(this);
    }
 
+ 
+   
+
+
+
+
    viderarticle(){
 
 
     this.setState({nombrearticle: 0})
   
   }
+
+
+
+
 ajoutarticle(){
 
 
   this.setState({nombrearticle: this.state.nombrearticle+1})
 
 }
+
+
 retirarticle(){
 
 if (this.state.nombrearticle > 0){
   this.setState({nombrearticle: this.state.nombrearticle-1})
 
-}
-
-  
-  
+} 
   }
+
+
 
 
 
    render() {
       return (
+
          <div>
 <p>article dans mon painer {this.state.nombrearticle}</p>
             
@@ -62,7 +74,7 @@ if (this.state.nombrearticle > 0){
             <button onClick={this.ajoutarticle}>Ajouter 1 au panier</button>
             <button onClick={this.retirarticle}> retirer 1 au panier</button>
             <button onClick={this.viderarticle}> Vider panier</button>
-
+{this.Articleliste}
 
 
 
@@ -89,6 +101,7 @@ if (this.state.nombrearticle > 0){
   <input type="submit" value="Envoyer" />
 </form>
          </div>
+
       );
    }
 }
