58040213ffddb7bf794fc99ebea38b83826123e6
lamisy93 <ami.sy@hotmail.fr>
2019-07-09 17:17:00 +0200

    'update'

---
+++ src/components/PeanutBasket/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import './styles.css'
+
+class PeanutBasket extends Component {
+    render() {
+      return (
+          <div>
+            <img alt='' src={require('./../../bonhomme_maq.png')}/>
+            <p>Hey, John</p>
+          </div>
+      )
+    }
+  }
+  
+  export default PeanutBasket
+  
